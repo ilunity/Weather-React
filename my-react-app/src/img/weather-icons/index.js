1828e8f6064657482d00650af5a49566879944d7
Ilya Shishkin <mostbook@mail.ru>
2022-03-27 13:20:21 +0700

    create index file for weather icons

---
+++ my-react-app/src/img/weather-icons/index.js
@@ -0,0 +1,17 @@
+import thunderstormIcon from './thunderstorm.png';
+import showerRainIcon from './shower-rain.png';
+import rainIcon from './rain.png';
+import snowIcon from './snow.png';
+import mistIcon from './mist.png';
+import clearSkyIcon from './clear-sky.png';
+import scatteredClouds from './scattered-clouds.png';
+
+export {
+    thunderstormIcon,
+        showerRainIcon,
+        rainIcon,
+        snowIcon,
+        mistIcon,
+        clearSkyIcon,
+        scatteredClouds
+}
