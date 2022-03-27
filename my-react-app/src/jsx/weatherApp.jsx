ee09301071c2c0f4b32a890b76c811f686e77dfc
Ilya Shishkin <mostbook@mail.ru>
2022-03-27 13:23:31 +0700

    App update. Configured all components to work with redux global store and localStorage

--- my-react-app/src/jsx/weatherApp.jsx
+++ my-react-app/src/jsx/weatherApp.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import '../css/weatherApp.css';
-import {Search} from "../components/search";
-import {ResultWindow} from "../components/resultWindow";
-import {FavouriteCitiesList} from "../components/favouriteCitiesList";
+import React, {useEffect} from 'react';
+import '../css/weatherAppStyle.css';
+import {Search, ResultWindow, FavouriteCitiesList} from "../components/index";
+import {useLoadStorage, useWeatherFetch} from '../components/customHooks/customHooks';
+import {useDispatch, useSelector} from "react-redux";
 
 function WeatherApp(props) {
+    const dispatch = useDispatch();
+    const currentCity = useSelector(store => store.currentCity);
+
+    useEffect(() => {
+        useLoadStorage(dispatch);
+    }, []);
+    useEffect(() => {
+        useWeatherFetch(dispatch, currentCity);
+    },[currentCity]);
+
     return (
-        <div>
+        <div className={'container'}>
             <Search/>
             <div className="main-window">
                 <ResultWindow/>
